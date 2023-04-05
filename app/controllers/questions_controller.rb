f5cec9b6ec388c220db6305ecfadb77671ac7355
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-04-05 19:51:18 +0900

    newのview

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,27 @@
 class QuestionsController < ApplicationController
 
+  require 'openai'
+
 
   def index
     
+  end
+  
+  def new
+    @question = Question.new
+  end
+  def create
+    if params[:job_type].present?
+      client = OpenAI::Client.new(api_key: ENV["OPENAI_API_KEY"])
+      prompt = "私がなりたい職業は #{job_type} です"
+      response = client.completions(
+      
+        engine: 'davinci',
+        prompt: params[:prompt],
+        max_tokens: 5
+      )
+
+      @generated_text = response.choices[0].text
+    end
   end
 end
