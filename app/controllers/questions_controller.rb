fe048d769838ab4b2d1ba552e17008b82bdebf15
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-04-06 08:30:49 +0900

    response　ok

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,16 +13,22 @@ class QuestionsController < ApplicationController
 
   def create
     @question = Question.new(question_params)
-    if @question.save
-      client = OpenAI::Client.new(api_key: ENV["OPENAI_API_KEY"])
-      prompt = "言語は #{params[:question][:language]} です。"
-      response = client.completions(
-        engine: 'davinci',
-        prompt: prompt,
-        max_tokens: 5
-      )
+    client = OpenAI::Client.new(api_key: ENV["OPENAI_API_KEY"])
+    prompt = "言語は #{params[:question][:language]} です。"
+    response = client.completions(
+      engine: 'curie',
+      prompt: prompt,
+      max_tokens: 5
+    )
 
-      @generated_text = response.choices[0].text
-    end
+    @generated_text = response.choices[0].text
+    redirect_to root_path
   end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:language, :framework, :hobby, :former_job, :desired_engineer)
+  end
+
 end
