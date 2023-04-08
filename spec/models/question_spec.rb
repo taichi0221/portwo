34f04526faa4319968be0c8337f73ddcbcd212b6
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-04-08 15:56:10 +0900

    question test

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -13,5 +13,26 @@ RSpec.describe Question, type: :model do
       @question.valid?
       expect(@question.errors.full_messages).to include "Language can't be blank"
     end
+    it 'frameworkが空では登録できない' do
+      @question.framework = ''
+      @question.valid?
+      expect(@question.errors.full_messages).to include "Framework can't be blank"
+    end
+    it 'hobbyが空では登録できない' do
+      @question.hobby = ''
+      @question.valid?
+      expect(@question.errors.full_messages).to include "Hobby can't be blank"
+    end
+    it 'desired_engineerが空では登録できない' do
+      @question.desired_engineer = ''
+      @question.valid?
+      expect(@question.errors.full_messages).to include "Desired engineer can't be blank"
+    end
+    it 'user_idが空では登録できない' do
+      @question.user_id = ''
+      @question.valid?
+      expect(@question.errors.full_messages).to include "User must exist"
+    end
+    
   end
 end
