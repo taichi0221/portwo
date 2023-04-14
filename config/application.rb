477a31eccaa5de7dd4d76b09d9c3c9d95bf5d1b0
taichi muta <taichi.wwr@gmail.com>
2023-04-14 19:02:14 +0900

    Update

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module PortHelper
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.logger = ActiveSupport::Logger.new("log/#{Rails.env}.log")
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
